f3cf07a9452a21eea964a2e3d1d2f70be406d2d6
Estevam <estevamfurtado@gmail.com>
2022-09-06 14:09:11 -0300

    fix: login

--- src/contexts/DeckContext/index.tsx
+++ src/contexts/DeckContext/index.tsx
@@ -1,5 +1,6 @@
-import { createContext, PropsWithChildren } from "react"
+import { createContext, PropsWithChildren, useContext, useEffect } from "react"
 import useDeck, { initialDeck } from "../../hooks/useDeck"
+import { AuthContext } from "../AuthContext"
 
 type ContextValues = {
     deckData: ReturnType<typeof useDeck>
@@ -11,7 +12,16 @@ export const DeckContext = createContext<ContextValues>({
 
 export function DeckProvider ({ children }: PropsWithChildren) {
 
+    const {authData: {data: {token}}} = useContext(AuthContext)
+
     const deckData = useDeck();
+
+    useEffect(()=>{
+        if (token) {
+            console.log('mudou o token')
+            deckData.actions.updateDeck();
+        }
+    },[token])
 
     const values : ContextValues = {deckData}
 
